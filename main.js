1212ea677a76870e1bf9fca3f05f7f09ad87f63e
power-amin <power.ameen99@gmail.com>
2018-09-17 13:15:41 +0300

    boxGame

---
+++ main.js
@@ -0,0 +1,118 @@
+let colors = [
+    '#C94C24',
+    '#C4226F',
+    '#859835',
+    '#6DA3637',
+    '#F0AD4E',
+    '#1fad39',
+    '#377ad2',
+    '#831cbf'
+];
+let manyBoxes = 63;
+makeBoxes(manyBoxes);
+let divLength;
+
+// This will create boxes with random colors
+function makeBoxes(boxesNumber) {
+    let element;
+    let myNode = document.querySelector('.boxes');
+    for (let i = 0; i < boxesNumber; i++) {
+        let colorString = colors[Math.floor(Math.random() * colors.length)]
+        element = document.createElement("div");
+        element.className = 'box';
+        element.style = "background-color:" + colorString;
+        element.setAttribute("data-id", i);
+        myNode.appendChild(element);
+    }
+
+}
+
+let previousElement = null;
+// When you click to hide the box
+$('.boxes').on("click", ".box", function(e) {
+
+    divLength = $('div.box').length;
+
+    // chick if the element that you clicked on it is null
+    if (previousElement != null) {
+
+        // Then we check if you are clicking the same element to time
+        if($(previousElement).attr('data-id') != $(this).attr('data-id')) {
+
+            // Then we check if you clicked on the same color to hide it
+            if($(previousElement).css("background-color") == $(this).css("background-color")) {
+
+                $(previousElement).fadeOut(350, function() {
+                    $(this).remove();
+                });
+                $(this).fadeOut(350, function() {
+                    $(this).remove();
+                });
+                 previousElement = null;
+                 return false;
+            }
+            $(previousElement).removeClass("selected");
+        }
+    }
+    $(this).addClass("selected");
+    previousElement = this;
+});
+
+
+let timer = 60; // 1 minute timer
+let min = 0;    // 1 minute
+let sec = 0;    // 1 second timer
+let hour = 0;   // 1 hour timer
+
+// Start count down
+function startTimer() {
+
+    min = parseInt(timer/60);
+    sec = parseInt(timer%60);
+
+    if (timer <= 20) {
+        $("#time").removeClass("text-success");
+        $("#time").addClass('text-danger');
+    }
+    if (timer <= 0) {
+      if(divLength >= 30) {
+        modalsResult("#one-star");
+      }
+      if (divLength < 30 && divLength >= 20){
+        modalsResult("#tow-stars");
+      }
+      if(divLength <= 10) {
+        modalsResult("#three-stars");
+      }
+        return;
+    }
+    document.getElementById('time').innerHTML = '<b>Time left: </b>' + hour.toString() + " : " + min.toString() + " : " + sec.toString();
+    timer--;
+    setTimeout(function() {
+        startTimer();
+    }, 1000);
+}
+
+function modalsResult(result) {
+   $(result).modal({
+        keyboard: false,
+        backdrop : false,
+   });
+   $(result).modal('show');
+   $(".btn-start-again").click(function() {
+     location.reload();
+   });
+}
+
+$('#memberModal').modal({
+  keyboard: false,
+  backdrop : false,
+});
+$('#memberModal').modal('show');
+$(".btn-close").click(function() {
+    startTimer();
+});
+
+
+
+
